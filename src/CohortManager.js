64b2c47c999304eb41794ac55ed2d345b020fb5e
Enky <enkh6at@yahoo.com>
2022-12-27 00:01:07 +0000

    adding some more function

--- src/CohortManager.js
+++ src/CohortManager.js
@@ -54,6 +54,38 @@ class Cohortmanager {
     }
     throw new Error(`Cohort ${name} doesn't exist`)
   }
+  searchStudentId(id) {
+    if (typeof id !== 'number') {
+      return
+    }
+    const searchedStudentId = this.studentList.find(
+      (student) => student.id === id
+    )
+    if (searchedStudentId) {
+      return searchedStudentId
+    }
+  }
+  searchStudentName(name) {
+    if (typeof name !== 'string') {
+      return
+    }
+    const result = []
+    const searchedStudentName = this.studentList.filter((student) =>
+      student.name.includes(name)
+    )
+    const searchedStudentSurname = this.studentList.filter((student) =>
+      student.surname.includes(name)
+    )
+    if (searchedStudentName !== undefined) {
+      result.push(...searchedStudentName)
+    }
+    if (searchedStudentSurname !== undefined) {
+      result.push(...searchedStudentSurname)
+    }
+    if (searchedStudentName || searchedStudentSurname) {
+      return result
+    }
+  }
 }
 
 module.exports = Cohortmanager
